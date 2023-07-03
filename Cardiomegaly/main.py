ee8434a0cd430c8a19c85343c27c30348e0fc74d
Naima Hannan <naima.hannan123@gmail.com>
2023-07-03 16:56:26 +0100

    Day1: making a button to open an image from file explorer

--- Cardiomegaly/main.py
+++ Cardiomegaly/main.py
@@ -1,7 +1,28 @@
+import tkinter as tk
+from tkinter import filedialog
+from PIL import ImageTk, Image
 
-def main():
-    print("Hello World!")
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png *.jpg *.jpeg")])
+    if file_path:
+        image = Image.open(file_path)
+        photo = ImageTk.PhotoImage(image)
+        image_label.config(image=photo)
+        image_label.image = photo
 
 
-if __name__ == "__main__":
-    main()
+
+# Create the main window
+window = tk.Tk()
+window.title("Cardiomegaly Detector")
+
+# Create a button to open a new window
+button = tk.Button(window, text="Open Image", command=open_file)
+button.pack()
+
+#creating a label to store the image
+image_label = tk.Label(window)
+image_label.pack()
+
+# Run the main event loop
+window.mainloop()
