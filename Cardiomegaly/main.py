c323d510c5d129cce3acf5a8e454c5ce427e213f
Naima Hannan <naima.hannan123@gmail.com>
2023-07-06 16:01:36 +0100

    Cleaned up the layout of the code

--- Cardiomegaly/main.py
+++ Cardiomegaly/main.py
@@ -14,16 +14,23 @@ def open_image():
         canvas.create_image(0, 0, anchor="nw", image=image)
         canvas.image = image  # Save a reference to the image to prevent it from being garbage collected
 
+
+
 # What happens when the mouse is pressed down on the image for the heart diameter
 def start_drawing_hline():
     canvas.bind("<Button-1>", start_hline)
     canvas.bind("<ButtonRelease-1>",calculate_Hdistance)
+    
+    
 
+# Calculates the distance of the heart diameter (in pixels)
 def calculate_Hdistance(event):
     x1, y1 = line1_start_x, line1_start_y
     x2, y2 = event.x, event.y
     Hdistance = sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    Hdistance_label.config(text=f"Heart Distance: {Hdistance:.2f} px")    
+    Hdistance_label.config(text=f"Heart Distance: {Hdistance:.2f} px")
+
+    return Hdistance    
 
 # What happens when the mouse is dragged along the image for the heart diameter
 def start_hline(event):
@@ -37,22 +44,27 @@ def draw_hline(event):
     canvas.delete("line1")
     canvas.create_line(line1_start_x, line1_start_y, event.x, event.y, tags="line1", fill="yellow", width=2)
     
-
 # Displays the co-ordinates of where the heart diameter starts and ends
-
     Hcoordinates_label.config(text=f"Heart-  Start: ({line1_start_x},{line1_start_y}) End: ({event.x},{event.y})")
+
+
+
 
 # What happens when the mouse is pressed down on the image for the lung diameter
 def start_drawing_Lline():
     canvas.bind("<Button-1>", start_Lline)
     canvas.bind("<ButtonRelease-1>",calculate_Ldistance)
+    
+    
 
 # Calculates lung distance
 def calculate_Ldistance(event):
     x1, y1 = line2_start_x, line2_start_y
     x2, y2 = event.x, event.y
     Ldistance = sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    Ldistance_label.config(text=f"Lungs Distance: {Ldistance:.2f} px")  
+    Ldistance_label.config(text=f"Lungs Distance: {Ldistance:.2f} px") 
+    
+    return Ldistance 
 
 # What happens when the mouse is dragged along the image for the lung diameter
 def start_Lline(event):
@@ -67,8 +79,14 @@ def draw_Lline(event):
     canvas.create_line(line2_start_x, line2_start_y, event.x, event.y, tags="line2", fill="red", width=2)
 
 # Displays the co-ordinates of where the lung diameter starts and ends
-
     Lcoordinates_label.config(text=f"Lungs-  Start: ({line2_start_x},{line2_start_y}) End: ({event.x},{event.y})")
+
+
+#Calculates the ratio
+def calculate_ratio(Hdistance, Ldistance):
+    ct_ratio= float(Hdistance/Ldistance)
+    ct_ratio_label.config(text=f"Cardiothoraic Ratio: {ct_ratio:.2f}")
+    canvas.bind("<ButtonRelease-1>",calculate_ratio)
 
 # Deletes the lines drawn
 def stop_drawing():
@@ -78,7 +96,8 @@ def stop_drawing():
     canvas.delete("line2")
     Lcoordinates_label.config(text="")
     Hcoordinates_label.config(text="")
-
+    Ldistance_label.config(text="")
+    Hdistance_label.config(text="")
 
   
 
@@ -89,7 +108,6 @@ window = tk.Tk()
 window.title("Cardiomegaly Detector")
 window.state("zoomed")
 
-
 # Creates a frame for the buttons
 button_frame= tk.Frame(window,bg="lightgray", width=400)
 button_frame.pack(side=tk.LEFT, fill=tk.BOTH)
@@ -98,48 +116,51 @@ button_frame.pack(side=tk.LEFT, fill=tk.BOTH)
 canvas = tk.Canvas(window)
 canvas.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
 
+
+
+
 # Create a button to open a new window for file explorer
 button = tk.Button(button_frame, text="Open Image", command=open_image)
-button.pack()
 button.place(x=50, y=100)
 button.configure(bg="#797EF6")
 
 # Creates a button to draw the heart diameter
 start_hbutton= tk.Button(button_frame,text="Draw Heart Diameter",command=start_drawing_hline)
-start_hbutton.pack()
 start_hbutton.configure(bg="#797EF6")
 start_hbutton.place(x=50, y=250)
 
 # Creates a button to draw the lung diameter
 start_Lbutton= tk.Button(button_frame,text="Draw Lung Diameter",command=start_drawing_Lline)
-start_Lbutton.pack()
 start_Lbutton.configure(bg="#797EF6")
 start_Lbutton.place(x=50, y=290)
 
 # Creates a button to clear the canvas
 stop_button = tk.Button(button_frame, text="Clear Canvas", command=stop_drawing)
-stop_button.pack()
 stop_button.configure(bg="#D31A38")
 stop_button.place(x=50, y=330)
+
+# Creates a button to calculate the cardiothoraic ratio
+calculate_button= tk.Button(button_frame, text="Calculate", command=calculate_ratio)
+calculate_button.place(x=50,y=150)
+
+
 
 
 #Creates a label for the heart co-ordinates
 Hcoordinates_label = tk.Label(button_frame, text="", font=("Verdana",8),bg="lightgray")
-Hcoordinates_label.pack()
 Hcoordinates_label.place(x=50,y=800)
 
 # Creates a label for the heart co-ordinates
 Lcoordinates_label = tk.Label(button_frame, text="", font=("Verdana",8),bg="lightgray")
-Lcoordinates_label.pack()
 Lcoordinates_label.place(x=50,y=820)
 
 # Creates a label to display the distance of the heart
-Hdistance_label= tk.Label(button_frame, text="", font=("Verdana",12),bg="lightgray")
+Hdistance_label= tk.Label(button_frame, text="", font=("Verdana",10),bg="lightgray")
 Hdistance_label.place(x=50, y=500)
 
 #  Creates a label to display the distance of the lungs
-Ldistance_label= tk.Label(button_frame, text="", font=("Verdana",12),bg="lightgray")
-Ldistance_label.place(x=50, y=550)
+Ldistance_label= tk.Label(button_frame, text="", font=("Verdana",10),bg="lightgray")
+Ldistance_label.place(x=50, y=530)
 
 # Creates a label to show the user what its called lmao
 title_label= tk.Label(button_frame,text="Cardiomegaly Detector", font=("Verdana",12),bg="lightgray")
@@ -150,8 +171,16 @@ subheader_label= tk.Label(button_frame,text="Drawing Tools", font=("Verdana",12)
 subheader_label.place(x=50,y=200)
 
 # Creates a label to show the user the co-ordinates
-coordinates_label= tk.Label(button_frame,text="Co-ordinates",font=("Verdana",12),bg="lightgray")
+coordinates_label= tk.Label(button_frame,text="Co-ordinates:",font=("Verdana",12),bg="lightgray")
 coordinates_label.place(x=50,y=750)
+
+#Creates a label to show the user the distances
+distances_label=tk.Label(button_frame,text="Distances:",font=("Verdana",12),bg="lightgray")
+distances_label.place(x=50,y=470)
+
+# Creates a label to show the user the cardiothoraic ratio
+ct_ratio_label= tk.Label(button_frame, text="", font=("Verdana",10),bg="lightgray")
+ct_ratio_label.place(x=50, y=600)
 
 # Run the main event loop
 window.mainloop()
